58f5afa2dc028de1196e1b12ffaefab81880d831
Mayur Parmar <mrparmar>
2019-11-06 14:33:40 +0530

    Initial Commit

---
+++ Quizzler/question.swift
@@ -0,0 +1,26 @@
+//
+//  question.swift
+//  Quizzler
+//
+//  Created by Agstya Technologies on 19/09/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText :String
+    let answer : Bool
+    
+    init(text:String, correctAnswer:Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+    
+}
+
+
+
+
+
